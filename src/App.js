cb34f440719d001837a28e4989d77b94bba449ee
Bharati Subramanian <bharatisharada@gmail.com>
2021-12-30 22:44:34 +0530

    feat: intro page completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import IntroPage from './components/IntroPage';
 
 function App() {
+  const [startQuiz, setStartQuiz] = useState(false);
+
+  function changeQuizState(){
+    setStartQuiz(true);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="main">
+      {
+        startQuiz ? 
+        <div className='section-wrapper'></div>
+        :
+        <div className='section-wrapper'>
+          <IntroPage startQuizHandler={changeQuizState} />
+        </div>
+      }
+    </main>
   );
 }
 
---
+++ src/components/IntroPage.js
@@ -0,0 +1,11 @@
+export default function IntroPage(props) {
+    return (
+        <section className="intro-page">
+            <h1 className="intro-page--head">Quizzical</h1>
+            <p className="intro-page--lead">Test your love for Books!</p>
+            <button className="btn btn-primary" onClick={props.startQuizHandler}>
+                Start Quiz
+            </button>
+        </section>
+    )
+}
