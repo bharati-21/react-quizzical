5d08e3ad8f0479d6d9b24234e84c54536b2bac47
Bharati Subramanian <bharatisharada@gmail.com>
2021-12-31 12:38:08 +0530

    refactor: btn class name

--- src/components/IntroPage.js
+++ src/components/IntroPage.js
@@ -3,7 +3,7 @@ export default function IntroPage(props) {
         <section className="intro-page">
             <h1 className="intro-page--head">Quizzical</h1>
             <p className="intro-page--lead">Test your love for Books!</p>
-            <button className="btn btn-primary" onClick={props.startQuizHandler}>
+            <button className="btn btn-start-quiz" onClick={props.startQuizHandler}>
                 Start Quiz
             </button>
         </section>
