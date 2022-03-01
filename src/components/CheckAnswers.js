4cccd99dc68620d5fb0ab71a0fb0d4a942830662
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-01 20:59:48 +0530

    refactor: logic for check answer

--- src/components/CheckAnswers.js
+++ src/components/CheckAnswers.js
@@ -1,22 +1,12 @@
-import { useEffect } from "react";
 
 export default function CheckAnswers(props) {
-    const {options, numCorrectAnswers} = props;
-
-    useEffect(() => {
-        let numCorrect = 0;
-
-        options.forEach(option => {
-            numCorrect += option.answeredCorrect ? 1 : 0;
-        })
-        props.setNumCorrectAnswers(numCorrect);
-    }, [props, options]) 
+    const {numCorrectAnswers, handleRetakeQuiz} = props;
 
 
     return (
         <div className="quiz-ended-wrapper">
             <p className="score">You answered <strong>{numCorrectAnswers}/10</strong> questions correctly</p>
-            <button className='btn btn-check' onClick={props.checkAnswers}>Retake Quiz</button>
+            <button className='btn btn-check' onClick={handleRetakeQuiz}>Retake Quiz</button>
         </div>
     )
 }
