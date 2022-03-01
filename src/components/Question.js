d9e0342906188fc279863d68b0f16a317532c13d
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-01 22:22:51 +0530

    refactor: question and options, classes

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,25 +1,20 @@
-export default function Question(props) {
-    const {question, id, handleUserSelection, options, quizEnded} = props; 
-    
-   
+export default function Question({quizItem, handleUserSelection, quizEnded}) {
     let questionClassNames = 'question';
+    
+    const {id, question, options, correctAnswer} = quizItem;
+
     if(quizEnded) {
-        if(options.answeredCorrect) {
-            questionClassNames+=' answered-correct';
-        }
-        else questionClassNames+= ' answered-incorrect';
+        const selectedOption = options.find(option => option.isSelected);
+        questionClassNames += (selectedOption.option === correctAnswer) ? ' answered-correct' : ' answered-incorrect'
     }
 
     return (
-        <article className={questionClassNames} id={id} onClick={handleUserSelection}>
+        <article className={questionClassNames} id={id}>
             <h2 className="question--text">{atob(question.toString())}</h2>
             <div className="question--options-container">
                 {
-                    
-                    options.options.map((option,index) => {
-
-                        
-                        return <button className={`option ${quizEnded && option.correctAnswer ? 'correct-answer' : option.incorrectAnswer ? 'incorrect-answer' :''} ${option.isSelected ? 'selected' : ''}`} key={`${id}-${index}`} value={atob(option.option)}>{atob(option.option)}</button>
+                    options.map((option,index) => {
+                        return <button className={`option ${option.isSelected ? 'selected' : ''} ${option === correctAnswer ? 'correct-answer' : ''}`} value={atob(option.option)} onClick={e => handleUserSelection(e, id)}>{atob(option.option)}</button>
                     })
                 }
             </div>
